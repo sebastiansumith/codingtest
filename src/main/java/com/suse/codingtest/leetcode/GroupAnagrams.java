b673157cc357fe2a3357836e1f75639b6c78a10b
Sumith Sebastian <sebastiansumith@gmail.com>
2020-04-07 11:30:05 -0500

    Counting Elements

--- src/main/java/com/suse/codingtest/leetcode/GroupAnagrams.java
+++ src/main/java/com/suse/codingtest/leetcode/GroupAnagrams.java
@@ -6,25 +6,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Given an array of strings, group anagrams together.
+ *
+ *     Example:
+ *
+ *     Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ *     Output:
+ *     [
+ *     ["ate","eat","tea"],
+ *     ["nat","tan"],
+ *     ["bat"]
+ *     ]
+ *     Note:
+ *
+ *     All inputs will be in lowercase.
+ *     The order of your output does not matter.
+ */
 public class GroupAnagrams {
-
-    /*
-    Given an array of strings, group anagrams together.
-
-    Example:
-
-    Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
-    Output:
-    [
-    ["ate","eat","tea"],
-    ["nat","tan"],
-    ["bat"]
-    ]
-    Note:
-
-    All inputs will be in lowercase.
-    The order of your output does not matter.
-    */
 
     public static void main(String[] args) {
         String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
